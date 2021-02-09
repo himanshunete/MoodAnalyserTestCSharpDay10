a8b1a92ab91c5d07b68339cc08b399d769ceea1f
himanshunete <himanshuneteh@gmail.com>
2021-02-09 13:29:40 +0530

    [himanshunete]Add FirstCommit

---
+++ MoodAnalyserTest/MoodAnalyser.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MoodAnalyserTest
+{ 
+    
+    public class MoodAnalyser
+    {
+        string message;
+        /// <summary>
+        /// setting parameterised constructor with sad or happy message
+        /// </summary>
+        /// <param name="message"></param>
+
+        public MoodAnalyser(string message)
+        {
+            this.message = message;
+        }
+
+        public string AnalyserMethod()
+        {
+            if (this.message.Contains("SAD"))
+                return "SAD";
+            else
+                return "HAPPY";
+        }
+    }
+}
---
+++ NUnitTestProject1/UnitTest1.cs
@@ -0,0 +1,28 @@
+using NUnit.Framework;
+using MoodAnalyserTest;
+
+namespace NUnitTestProject1
+{
+    public class Tests
+    {
+        MoodAnalyser moodAnalyser;
+        [SetUp]
+        public void Setup()
+        {
+            moodAnalyser = new MoodAnalyser("SAD");
+        }
+
+
+        /// <summary>
+        /// Given,When,Outcome
+        /// </summary>
+        [Test]
+        public void Given_SadMood_Expecting_Sad_Result()
+        {
+            string expected = "SAD";
+
+            string actual = moodAnalyser.AnalyserMethod();
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
